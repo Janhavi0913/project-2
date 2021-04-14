71ac66bf058003310b58d1a594934662f8029e05
Janhavi0913 <61356171+Janhavi0913@users.noreply.github.com>
2021-04-14 11:27:55 -0400

    Update analysis.c

--- analysis.c
+++ analysis.c
@@ -19,18 +19,31 @@ typedef struct wordnode
 	struct wordnode* next;
 } wordnode;
 
-wordnode* file;
-double WFD;
-char* filename;
-
-wordnode* createNode(char* word)
+typedef struct filenode
 {
+	char* filename;
+	wordnode* head;
+	double totalnodes;
+	struct filenode* next;
+} filenode;
+
+wordnode* createNode(char* word){ // this would have to call insert method
         struct wordnode* newnode = (struct wordnode*)malloc(sizeof(struct wordnode));
         newnode->word = word;
         newnode->numoccur = 1;
         newnode->totalnodes = 1;
+	newnode->WFD = 0;
         newnode->next = NULL;
         return newnode;
+}
+
+filenode* createFileNode(char* filename, wordnode* head, int totalnodes) // make this a void* return, and change to accept struct as argument
+{
+	struct filenode* thenode = (struct filenode*)malloc(sizeof(struct filenode));
+	thenode->filename = filename; // dequeue from the file queue
+	thenode->head = head; // call method that makes the wordnode for that file
+	thenode->totalnodes = totalnodes;
+	return thenode;
 }
 
 void freeNodes(wordnode* head)
@@ -46,7 +59,7 @@ void printLinkedlist(wordnode* head)
 	wordnode* ptr = head;
     while(ptr != NULL)
     {
-        printf("%s: %d\n", ptr->word, ptr->numoccur);
+        printf("%s\n", ptr->word);
         ptr = ptr->next;
     }
 }
@@ -57,6 +70,7 @@ wordnode* createCombined(wordnode* file1, wordnode* file2)
 	wordnode* ptr1;
 	wordnode* ptr2;
 	wordnode* fptr;
+	wordnode* prev;
 	ptr1 = file1->next;
 	ptr2 = file2;
 	fhead = createNode(file1->word);
@@ -70,40 +84,48 @@ wordnode* createCombined(wordnode* file1, wordnode* file2)
 		fptr = fptr->next;
 		ptr1 = ptr1->next;
 	}
+	fptr->next = NULL;
 	fptr = fhead;
 	while(ptr2 != NULL)
 	{
 		if(strcmp(fptr->word, ptr2->word) == 0)
 		{
 			fptr->numoccur += ptr2->numoccur;
+			ptr2 = ptr2->next;
 			if(fptr->next != NULL)
+			{	
+				prev = fptr;
 				fptr = fptr->next;
-			if(ptr2->next != NULL)
-				ptr2 = ptr2->next;
+			}
 			else
+			{
 				break;
+			}
 		}
 		else if(strcmp(fptr->word, ptr2->word) < 0)
 		{
-			if(ptr2->next != NULL)
-				ptr2 = ptr2->next;
+			if(fptr->next != NULL)
+			{
+				prev = fptr;
+				fptr = fptr->next;
+			}
 			else
 				break;
 		}
 		else
 		{
-						printf("Problem within here\n");
-
 			wordnode* thenode = createNode(ptr2->word);
 			thenode->numoccur = ptr2->numoccur;
 			thenode->next = fptr;
 			if(fptr == fhead)
-				thenode->totalnodes = fptr->totalnodes;
-			fptr = fptr->next;
-			if(ptr2->next != NULL)
-				ptr2 = ptr2->next;
+			{
+				thenode->totalnodes = fhead->totalnodes;
+				fhead = thenode;
+			}
 			else
-				break;
+				prev->next = thenode;
+			fptr = thenode;
+			ptr2 = ptr2->next;
 		}
 	}
 	if(ptr2 != NULL)
@@ -112,18 +134,14 @@ wordnode* createCombined(wordnode* file1, wordnode* file2)
 		{
 			fptr->next = ptr2;
 			fptr = fptr->next;
-			if(ptr2->next != NULL)
-				ptr2 = ptr2->next;
-			else
-				break;
+			ptr2 = ptr2->next;
 		}
 	}
 	fptr = fhead;
 	while(fptr != NULL)
 	{
-		fptr->WFD = fptr->numoccur/fhead->totalnodes;
-		if(fptr ->next != NULL)
-			fptr = fptr->next;
+		fptr->WFD = (double)fptr->numoccur/(double)fhead->totalnodes;
+		fptr = fptr->next;
 	}
 	return fhead;
 }
@@ -161,21 +179,26 @@ int main(int argc, char **argv)
 	f1->numoccur = 2;
 	f1->totalnodes = 4;
 	f11->numoccur = 2;
+	//filenode* file1 = createFileNode("f1.txt", f1, f1->totalnodes);
 
 	wordnode* f2 = createNode("apple");
 	wordnode* f21 = createNode("hi");
 	wordnode* f22 = createNode("out");
 	wordnode* f23 = createNode("there");
+	wordnode* f24 = createNode("z");
 	f2->next = f21;
 	f21->next = f22;
 	f22->next = f23;
+	f23->next = f24;
 	f2->numoccur = 1;
-	f2->totalnodes = 5;
+	f2->totalnodes = 6;
 	f21->numoccur = 2;
 	f22->numoccur = 1;
 	f23->numoccur = 1;
+	f24->numoccur = 1;
+	//filenode* file2 = createFileNode("f2.txt", f2, f2->totalnodes);
 
-	wordnode* result = createCombined(f1, f2);
+	wordnode* result = createCombined(f2, f1);
 	printLinkedlist(result);
 
 	freeNodes(f1);
